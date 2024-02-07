a61943a33d9156577931f32dc128d584e717134e
Ellen <ellenobrien34@gmail.com>
2024-02-07 12:16:15 -0500

    Added App Info activity and updated navigation to this activity from the menu button in the home navigation, weather table, and kickoff table.

--- app/src/main/java/com/ellenkniceley/bloodbowlcompanion/App_Info_Page.java
+++ app/src/main/java/com/ellenkniceley/bloodbowlcompanion/App_Info_Page.java
@@ -3,7 +3,6 @@ package com.ellenkniceley.bloodbowlcompanion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
@@ -22,25 +21,17 @@ public class App_Info_Page extends AppCompatActivity {
         String appInfo = "App Info";
         title.setText(appInfo);
 
-        menuIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showMenu(v);
-            }
-        });
+        menuIcon.setOnClickListener(this::showMenu);
     }
     private void showMenu(View v){
         PopupMenu popupMenu = new PopupMenu(App_Info_Page.this,v);
         popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId()== R.id.appInfo)
-                    Toast.makeText(App_Info_Page.this, "You clicked app info", Toast.LENGTH_SHORT).show();
-                return true;
+        popupMenu.setOnMenuItemClickListener(item -> {
+            if(item.getItemId()== R.id.appInfo)
+                Toast.makeText(App_Info_Page.this, "You are already on the App Info page.", Toast.LENGTH_SHORT).show();
+            return true;
 
 
-            }
         });
         popupMenu.show();
     }
